e9d3a18b0f0d5b8e52dc9eb3510e95b3b63a3631
IvyW2 <ivywambugha2@gmail.com>
2023-03-08 15:57:46 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,48 @@
 # Your code here!
+# first method
+def greet_programmer
+    puts ("Hello, programmer!")
+end
+
+greet_programmer
+
+# second method
+def greet(name)
+    puts ("Hello, #{name}!")
+end
+
+greet("Naureen")
+
+
+# third method
+def greetWithDefault(name = "programmer")
+    puts("Hello, #{name}!")
+end
+
+greetWithDefault
+
+def greetWithDefault(name = "programmer")
+    puts("Hello, #{name}!")
+end
+
+greetWithDefault("Naureen")
+
+# forth method
+def add(num1, num2)
+    return (num1 + num2)
+end
+
+puts add(1, 2)
+
+
+# fifth method - control flow
+def halve(number)
+    if (number.class != Integer)
+        return nil
+    end
+    number / 2
+
+end
+
+puts halve(4)
+puts halve("two")
